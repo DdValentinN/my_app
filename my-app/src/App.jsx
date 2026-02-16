50af3bece0c6d73b4d271ef6b33241efb436a6c0
Valentin <delattre.v@outlook.fr>
2026-02-16 09:56:56 +0100

    chore: install react-router-dom

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,3 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+
 export default function App() {
-  return <h1>Test React</h1>;
+  return (
+    <BrowserRouter>
+      <div style={{ padding: 24, fontFamily: "system-ui", minHeight: "100vh", display: "flex", flexDirection: "column" }}>
+        
+        <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
+          <Link to="/">Accueil</Link>
+          <Link to="/about">À propos</Link>
+        </nav>
+
+        <div style={{ flex: 1 }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+
+        <Footer />
+
+      </div>
+    </BrowserRouter>
+  );
 }
